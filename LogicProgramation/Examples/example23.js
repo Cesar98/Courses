a049b1504fb91a635960394c4eb29ee0fa733dd9
cesar alvarez <cesar_zuco@hotmail.com>
2021-07-17 14:26:00 -0500

    Ya se pudo el 23

--- LogicProgramation/Examples/example23.js
+++ LogicProgramation/Examples/example23.js
@@ -1,10 +1,10 @@
 function upWord(textChain){
     let newChain = '';
     for(word of textChain.split(' ')){
-        for(char of word){
-            newChain += char == word.charAt(0) ? char.toUpperCase() : char;
-        }
-        newChain += ' ';
+       for (let i = 0; i < word.length; i++) {
+           newChain += i == 0 ? word.charAt(i).toUpperCase() : word.charAt(i);          
+       }
+       newChain += ' '; 
     }
     return newChain;
 }
