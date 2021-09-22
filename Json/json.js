99579ac43a9e181d2aad3e13bfbff3365ca2abb9
cesar alvarez <cesar_zuco@hotmail.com>
2021-09-21 23:35:02 -0500

    JSON Course Finished

--- Json/json.js
+++ Json/json.js
@@ -1,39 +1,31 @@
-var jokes = [{
-    "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-    "id": "1vvH7UHQR1aq0FI1yLQUhw",
-    "url": "https://api.chucknorris.io/jokes/1vvH7UHQR1aq0FI1yLQUhw",
-    "value": "The day after Chuck Norris was born, his afterbirth enlisted as a Navy Seal."
-}, {
-    "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-    "id": "3lnAWT0XTGGUOBKGUlUNNQ",
-    "url": "",
-    "value": "Chuck Norris is older than his own dad."
-}, {
-    "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-    "id": "8svMaNbBQ4-7QPBf8hhAKA",
-    "url": "",
-    "value": "Chuck Norris found what Bono was looking for."
-}, {
-    "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-    "id": "jWmHME3SR7iyur9tgaMaPA",
-    "url": "https://api.chucknorris.io/jokes/jWmHME3SR7iyur9tgaMaPA",
-    "value": "Chuck Norris understands every definition in the Oxford Thesaurus, except one; that on mercy."
-}];
-
 var list = document.getElementById('list');
 
-jokes.forEach(joke=>{
-    createIl(joke.icon_url, joke.value)
-})
+// https://github.com/toddmotto/public-apis#social
 
-function createIl(icon, text) {
+$(document).ready(function () {
+    // $.getJSON("http://api.icndb.com/jokes/random/15", function(jokes) {
+    //     jokes.value.forEach(joke => {
+    //         createIl(joke.id, joke.joke)
+    //     });
+    // })
+    $.ajax({
+        url: "http://api.icndb.com/jokes/random/15",
+        type: "GET",
+        dataType: "json",
+        success: function (jokes) {
+            jokes.value.forEach(joke => {
+                createIl(joke.id, joke.joke)
+            });
+        }
+    });
+});
+
+function createIl(id, text) {
     let li = document.createElement('li');
     let a = document.createElement('a');
 
-    a.href = icon;
-    a.innerText = text;
+    a.innerText = id + ': ' + text;
 
     li.append(a);
     list.append(li);
-
 }
