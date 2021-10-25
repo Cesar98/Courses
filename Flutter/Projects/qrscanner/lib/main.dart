fde52b25901ce6dd0fc0a6c2ffd0fac55616faae
Cesar 98 <cesar_zuco@hotmail.com>
2021-10-25 13:01:05 -0500

    home page created

--- Flutter/Projects/qrscanner/lib/main.dart
+++ Flutter/Projects/qrscanner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrscanner/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Qr Scanner',
       initialRoute: 'home',
       routes: {
-        'home' : '',
-        'mapa' : '',
+        'home' : ( _ ) => HomePage(),
       },
     );
   }
