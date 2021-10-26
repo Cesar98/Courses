25fda164f0fd64ed206dcb5fcd5e35e8b2b0ca07
Cesar 98 <cesar_zuco@hotmail.com>
2021-10-25 20:10:21 -0500

    Scan Qr code implementation

--- Flutter/Projects/qrscanner/lib/widgets/custom_floating_button.dart
+++ Flutter/Projects/qrscanner/lib/widgets/custom_floating_button.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 class CustomFloatingButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       tooltip: 'Scan QR code',
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancel', false, ScanMode.QR);
+
+        print(barcodeScanRes);
+      },
       child: Icon(Icons.camera_alt),
     );
   }
