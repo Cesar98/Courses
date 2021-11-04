1f9ecf3243728993380c271bbb4dbf1b11dad7d6
Cesar 98 <cesar_zuco@hotmail.com>
2021-11-04 09:06:04 -0600

    Details on the login form

--- Flutter/Projects/products_app/lib/pages/login_page.dart
+++ Flutter/Projects/products_app/lib/pages/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/providers/login_form_provider.dart';
 import 'package:products_app/ui/input_decorations.dart';
 
 import 'package:products_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -31,12 +33,14 @@ class LoginPage extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                _LoginForm(),
+                ChangeNotifierProvider(
+                    create: (_) => LoginFormProvider(), child: _LoginForm())
               ],
             )),
           ),
           SizedBox(height: size.width * .10),
-          Text('Register')
+          Text('Register'),
+          SizedBox(height: size.width * .25),
         ],
       ),
     )));
@@ -46,44 +50,78 @@ class LoginPage extends StatelessWidget {
 class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginFrom = Provider.of<LoginFormProvider>(context);
+
     return Container(
       child: Form(
+          key: loginFrom.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                'example@gmail.com', 'Email', Icons.alternate_email_rounded),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            decoration: InputDecorations.authInputDecoration(
-                'Password', 'Password', Icons.lock_outline_rounded),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          MaterialButton(
-            onPressed: () {},
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            disabledColor: Colors.grey,
-            color: Colors.purple,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-              child: Text('Login', style: TextStyle(color: Colors.white)),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-        ],
-      )),
+            children: [
+              TextFormField(
+                  autocorrect: false,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecorations.authInputDecoration(
+                      'example@gmail.com',
+                      'Email',
+                      Icons.alternate_email_rounded),
+                  onChanged: (value) => loginFrom.email = value,
+                  validator: (value) {
+                    String pattern =
+                        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                    RegExp regExp = new RegExp(pattern);
+
+                    if (value == '') return 'Fill the email field';
+
+                    return regExp.hasMatch(value ?? '')
+                        ? null
+                        : 'Email field does not match';
+                  }),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                  autocorrect: false,
+                  obscureText: true,
+                  decoration: InputDecorations.authInputDecoration(
+                      'Password', 'Password', Icons.lock_outline_rounded),
+                  onChanged: (value) => loginFrom.password = value,
+                  validator: (value) {
+                    if (value == '') return 'Fill the password field';
+
+                    return value!.length >= 6
+                        ? null
+                        : 'Password field does have to be more than 6 chars';
+                  }),
+              SizedBox(
+                height: 30,
+              ),
+              MaterialButton(
+                onPressed: loginFrom.isLoading
+                    ? null
+                    : () {
+                        FocusScope.of(context).unfocus();
+
+                        if (!loginFrom.isValidForm()) return null;
+
+                        Navigator.pushReplacementNamed(context, 'home');
+                        loginFrom.isLoading = true;
+                      },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                color: Colors.purple,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(loginFrom.isLoading ? 'Please wait' : 'Login',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          )),
     );
   }
 }
