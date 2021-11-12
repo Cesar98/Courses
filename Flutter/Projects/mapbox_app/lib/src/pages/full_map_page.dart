74331f61e103af7fb47c9b2a67f5784ed2074536
cesar_zuco@hotmail.com <cesar_zuco@hotmail.com>
2021-11-12 16:38:05 -0600

    Rectifng project

--- Flutter/Projects/mapbox_app/lib/src/pages/full_map_page.dart
+++ Flutter/Projects/mapbox_app/lib/src/pages/full_map_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-/* 
-pk: pk.eyJ1IjoiY3VhcmN1YXIiLCJhIjoiY2theXFzbHIyMGt6aDJ5bWg0MGd1ZHF0biJ9.E-iL_pNcCtK9K6yMzIvOhA
-*/
-
 class FullMapPage extends StatelessWidget {
 
   @override
