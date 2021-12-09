ee4499a4df623567a86cbb87965166b5da2711b3
cesar_zuco@hotmail.com <cesar_zuco@hotmail.com>
2021-12-08 18:38:48 -0600

    Agora implementation failed

---
+++ Flutter/Projects/streaming_testing_app/lib/main.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+const APP_ID = '87dcb60a3b44461c908ecf6629cbec36';
+const Token = '00687dcb60a3b44461c908ecf6629cbec36IAAWWLQsD2WASj9JTJyJ9nE/JZ5hFu0o1XWjEgw92bw95lfucZIAAAAAEABUa/P7fJmyYQEAAQB8mbJh';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _joined = false;
+  int _remoteUid = 0;
+  bool _switch = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Init the app
+  Future<void> initPlatformState() async {
+    await [Permission.camera, Permission.microphone].request();
+
+    // Create RTC client instance
+    RtcEngineContext context = RtcEngineContext(APP_ID);
+    var engine = await RtcEngine.createWithContext(context);
+
+    // Define event handling logic
+    engine.setEventHandler(RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed) {
+          print('joinChannelSuccess ${channel} ${uid}');
+          setState(() {
+            _joined = true;
+          });
+        }, userJoined: (int uid, int elapsed) {
+      print('userJoined ${uid}');
+      setState(() {
+        _remoteUid = uid;
+      });
+    }, userOffline: (int uid, UserOfflineReason reason) {
+      print('userOffline ${uid}');
+      setState(() {
+        _remoteUid = 0;
+      });
+    }));
+    // Enable video
+    await engine.enableVideo();
+    // Set channel profile as livestreaming
+    await engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
+    // Set user role as broadcaster
+    await engine.setClientRole(ClientRole.Broadcaster);        
+    // Join channel with channel name as first
+    await engine.joinChannel(Token, 'first', null, 0);
+  }
+
+
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter example app'),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: _switch ? _renderRemoteVideo() : _renderLocalPreview(),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _switch = !_switch;
+                    });
+                  },
+                  child: Center(
+                    child:
+                    _switch ? _renderLocalPreview() : _renderRemoteVideo(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Local preview
+  Widget _renderLocalPreview() {
+    if (_joined) {
+      return RtcLocalView.SurfaceView();
+    } else {
+      return Text(
+        'Please join channel first',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  // Remote preview
+ Widget _renderRemoteVideo() {
+    if (_remoteUid != 0) {
+      return RtcRemoteView.SurfaceView(
+        uid: _remoteUid,
+        channelId: "first",
+      );
+    } else {
+      return Text(
+        'Please wait remote user join',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+}
